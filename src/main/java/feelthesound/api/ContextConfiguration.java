a134a885b723783d7a794c9d8d58c4b0eeaa8d98
Antanas Bastys <antanas.bastys@tieto.com>
2016-05-14 17:51:27 +0300

    CORS support to allow all origins

--- src/main/java/feelthesound/api/ContextConfiguration.java
+++ src/main/java/feelthesound/api/ContextConfiguration.java
@@ -3,19 +3,22 @@ package feelthesound.api;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig extends SpringBootServletInitializer
+public class ContextConfiguration extends SpringBootServletInitializer
         implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(streamHandler(), "/streams/{identifier}");
-        registry.addHandler(listenerHandler(), "/listeners/{identifier}");
+        registry.addHandler(streamHandler(), "/streams/{identifier}").setAllowedOrigins("*");
+        registry.addHandler(listenerHandler(), "/listeners/{identifier}").setAllowedOrigins("*");
     }
 
     @Bean
@@ -26,5 +29,15 @@ public class WebSocketConfig extends SpringBootServletInitializer
     @Bean
     public ListenerHandler listenerHandler() {
         return new ListenerHandler();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("*").allowedOrigins("*");
+            }
+        };
     }
 }
